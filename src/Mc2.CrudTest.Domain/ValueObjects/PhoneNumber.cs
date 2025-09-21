c28677e8b92b002fecc435935d97fc1877855a9c
fzleli <amirfzlelii@gmail.com>
2025-09-21 18:15:08 +0330

    feat(domain): implement PhoneNumber value object and InvalidPhoneNumberException
    - Added PhoneNumber class with minimal validation rules
    - Added InvalidPhoneNumberException in Domain.Customers.Exceptions
    - All previously failing PhoneNumberTests now pass because the above files did not exist

--- src/Mc2.CrudTest.Domain/ValueObjects/PhoneNumber.cs
+++ src/Mc2.CrudTest.Domain/ValueObjects/PhoneNumber.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Mc2.CrudTest.Domain.Exceptions;
 
 namespace Mc2.CrudTest.Domain.ValueObjects
 {
-    internal class PhoneNumber
+    public class PhoneNumber
     {
+        public string Value { get; private set; }
+
+        private PhoneNumber(string value)
+        {
+            Value = value;
+        }
+
+        public static PhoneNumber Create(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new InvalidPhoneNumberException(value);
+
+            if (value.Length < 10)
+                throw new InvalidPhoneNumberException(value);
+
+            return new PhoneNumber(value);
+        }
+
+        public override string ToString() => Value;
     }
 }
---
+++ tests/Mc2.CrudTest.Domain.UnitTests/Customers/ValueObjects/PhoneNumberTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Mc2.CrudTest.Domain.Exceptions;
+using Mc2.CrudTest.Domain.ValueObjects;
+
+namespace Mc2.CrudTest.Domain.UnitTests.Customers.ValueObjects
+{
+    public class PhoneNumberTests
+    {
+        [Fact]
+        public void Create_ShouldReturnPhoneNumber_WhenValueIsValid()
+        {
+            // Arrange
+            var value = "+989123456789";
+
+            // Act
+            var phoneNumber = PhoneNumber.Create(value);
+
+            // Assert
+            phoneNumber.Value.Should().Be(value);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("123")]
+        public void Create_ShouldThrowInvalidPhoneNumberException_WhenValueIsInvalid(string invalidValue)
+        {
+            // Act
+            Action act = () => PhoneNumber.Create(invalidValue);
+
+            // Assert
+            act.Should().Throw<InvalidPhoneNumberException>();
+        }
+    }
+}
