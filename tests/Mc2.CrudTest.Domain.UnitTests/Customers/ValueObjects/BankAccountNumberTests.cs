a232909ca65e9d871aa08f0c526cba26cd113a4e
fzleli <amirfzlelii@gmail.com>
2025-09-21 19:16:07 +0330

    test(domain): add failing tests for Email and BankAccountNumber value objects

---
+++ tests/Mc2.CrudTest.Domain.UnitTests/Customers/ValueObjects/BankAccountNumberTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Mc2.CrudTest.Domain.Exceptions;
+using Mc2.CrudTest.Domain.ValueObjects;
+
+namespace Mc2.CrudTest.Domain.UnitTests.Customers.ValueObjects
+{
+    public class BankAccountNumberTests
+    {
+        [Fact]
+        public void Create_ShouldReturnBankAccountNumber_WhenValueIsValid()
+        {
+            // Arrange
+            var value = "1234567890123456";
+
+            // Act
+            var accountNumber = BankAccountNumber.Create(value);
+
+            // Assert
+            accountNumber.Value.Should().Be(value);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("12345")]
+        [InlineData("abcde12345")]
+        public void Create_ShouldThrowInvalidBankAccountNumberException_WhenValueIsInvalid(string invalidValue)
+        {
+            // Act
+            Action act = () => BankAccountNumber.Create(invalidValue);
+
+            // Assert
+            act.Should().Throw<InvalidBankAccountNumberException>();
+        }
+    }
+}
---
+++ tests/Mc2.CrudTest.Domain.UnitTests/Customers/ValueObjects/EmailTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Mc2.CrudTest.Domain.Exceptions;
+using Mc2.CrudTest.Domain.ValueObjects;
+
+namespace Mc2.CrudTest.Domain.UnitTests.Customers.ValueObjects
+{
+    public class EmailTests
+    {
+        [Fact]
+        public void Create_ShouldReturnEmail_WhenValueIsValid()
+        {
+            // Arrange
+            var value = "test@example.com";
+
+            // Act
+            var email = Email.Create(value);
+
+            // Assert
+            email.Value.Should().Be(value);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("invalid-email")]
+        [InlineData("missing@domain")]
+        public void Create_ShouldThrowInvalidEmailException_WhenValueIsInvalid(string invalidValue)
+        {
+            // Act
+            Action act = () => Email.Create(invalidValue);
+
+            // Assert
+            act.Should().Throw<InvalidEmailException>();
+        }
+    }
+}
