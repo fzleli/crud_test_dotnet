c92cc7bd90bf0acba6f74a0646163b0903bc6e10
fzleli <amirfzlelii@gmail.com>
2025-09-21 10:54:38 +0330

    chore: setup customer clean architecture skeleton

---
+++ src/Mc2.CrudTest.Application/Customers/Commands/CreateCustomer/CreateCustomerHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Application.Customers.Commands.CreateCustomer
+{
+    internal class CreateCustomerHandler
+    {
+    }
+}
