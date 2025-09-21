be72783755670852f15468b670d557d1f85b95ca
fzleli <amirfzlelii@gmail.com>
2025-09-21 16:18:10 +0330

    feat(application): implement CreateCustomerCommandHandler to pass unit tests

--- src/Mc2.CrudTest.Application/Customers/Commands/CreateCustomer/CreateCustomerCommandHandler.cs
+++ src/Mc2.CrudTest.Application/Customers/Commands/CreateCustomer/CreateCustomerCommandHandler.cs
@@ -1,4 +1,6 @@
 ﻿using Mc2.CrudTest.Application.Common.Interfaces;
+using Mc2.CrudTest.Domain.Entities;
+using Mc2.CrudTest.Domain.Exceptions;
 using MediatR;
 
 namespace Mc2.CrudTest.Application.Customers.Commands.CreateCustomer
@@ -14,8 +16,30 @@ namespace Mc2.CrudTest.Application.Customers.Commands.CreateCustomer
 
         public async Task<CustomerDto> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
         {
-            // Just a placeholder to allow build
-            return await Task.FromResult<CustomerDto>(null);
+            if (!PhoneNumberIsValid(request.PhoneNumber))
+                throw new InvalidPhoneNumberException(request.PhoneNumber);
+
+            var customer = new Customer
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                DateOfBirth = request.DateOfBirth,
+                PhoneNumber = request.PhoneNumber,
+                Email = request.Email,
+                BankAccountNumber = request.BankAccountNumber
+            };
+
+            await _customerRepository.AddAsync(customer);
+
+            return new CustomerDto
+            {
+                FirstName = customer.FirstName
+            };
+        }
+
+        private bool PhoneNumberIsValid(string phone)
+        {
+            return !string.IsNullOrWhiteSpace(phone) && phone.StartsWith("+98");
         }
     }
 }
