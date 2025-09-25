8000ae38b28162fe014fb2a95a87ec57fa68a029
fzleli <amirfzlelii@gmail.com>
2025-09-25 23:05:42 +0330

    chore: Clean up Blazor template and add basic solution structure for DDD layers

---
+++ src/Mc2.CrudTest.Presentation/Mc2.CrudTest.Presentation.Server/Program.cs
@@ -0,0 +1,47 @@
+﻿using Mc2.CrudTest.Application.Common.Interfaces;
+using Mc2.CrudTest.Application.Customers.Commands.CreateCustomer;
+using Mc2.CrudTest.Infrastructure.Persistence;
+using Mc2.CrudTest.Infrastructure.Repositories;
+using Mc2.CrudTest.Infrastructure.Services;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateCustomerCommand).Assembly));
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IEventStore, EventStore>();
+builder.Services.AddSingleton<IPhoneNumberValidator, PhoneNumberValidator>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapPost("/api/customers", async (IMediator mediator, [FromBody] CreateCustomerCommand command) =>
+{
+    try
+    {
+        var customer = await mediator.Send(command);
+        return Results.Created($"/api/customers/{customer.Id}", customer);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message, statusCode: 400);
+    }
+})
+.WithName("CreateCustomer")
+.WithOpenApi();
+
+app.Run();
+
