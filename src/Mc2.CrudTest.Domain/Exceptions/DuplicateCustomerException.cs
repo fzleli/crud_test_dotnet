d1e8a523d377ea5e692da8059eed27c46f5ab8cc
fzleli <amirfzlelii@gmail.com>
2025-09-25 23:13:07 +0330

    feat(domain): DuplicateCustomerException and DuplicateEmailException for uniqueness checks

---
+++ src/Mc2.CrudTest.Domain/Exceptions/DuplicateCustomerException.cs
@@ -0,0 +1,12 @@
+﻿namespace Mc2.CrudTest.Domain.Exceptions
+{
+    public class DuplicateCustomerException : Exception
+    {
+        public DuplicateCustomerException(string firstName, string lastName, DateTime dateOfBirth)
+        : base($"A customer with FirstName '{firstName}', LastName '{lastName}', and DateOfBirth '{dateOfBirth:yyyy-MM-dd}' already exists.")
+        {
+        }
+
+        public DuplicateCustomerException() : base("Duplicate customer based on name and date of birth.") { }
+    }
+}
---
+++ src/Mc2.CrudTest.Domain/Exceptions/DuplicateEmailException.cs
@@ -0,0 +1,10 @@
+﻿namespace Mc2.CrudTest.Domain.Exceptions
+{
+    public class DuplicateEmailException : Exception
+    {
+        public DuplicateEmailException(string email)
+        : base($"The email '{email}' is already in use.")
+        {
+        }
+    }
+}
