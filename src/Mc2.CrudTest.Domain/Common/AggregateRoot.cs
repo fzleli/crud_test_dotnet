27111b9a9f48e55b6054cd9ebaf711c583d62be7
fzleli <amirfzlelii@gmail.com>
2025-09-25 23:09:33 +0330

    feat(domain): AggregateRoot, DomainEvent, ValueObject, and delete BaseEntity

---
+++ src/Mc2.CrudTest.Domain/Common/AggregateRoot.cs
@@ -0,0 +1,19 @@
+﻿namespace Mc2.CrudTest.Domain.Common;
+
+public abstract class AggregateRoot<TKey>
+{
+    public TKey Id { get; protected set; }
+
+    private readonly List<DomainEvent> _domainEvents = new List<DomainEvent>();
+    public IReadOnlyCollection<DomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void AddDomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+}
---
+++ src/Mc2.CrudTest.Domain/Common/DomainEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace Mc2.CrudTest.Domain.Common;
+
+public abstract class DomainEvent
+{
+    public DateTime OccurredOn { get; } = DateTime.UtcNow;
+}
