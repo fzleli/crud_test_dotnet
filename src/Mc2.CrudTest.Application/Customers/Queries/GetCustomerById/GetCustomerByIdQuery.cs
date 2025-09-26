0250ba691e136933cc4067472226b9097ea0a9d6
fzleli <amirfzlelii@gmail.com>
2025-09-26 19:06:55 +0330

    feat(application): add GetCustomerByIdQuery and handler

---
+++ src/Mc2.CrudTest.Application/Customers/Queries/GetCustomerById/GetCustomerByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using Mc2.CrudTest.Domain.Entities;
+using MediatR;
+
+namespace Mc2.CrudTest.Application.Customers.Queries.GetCustomerById;
+
+public record GetCustomerByIdQuery(Guid Id) : IRequest<Customer>;
