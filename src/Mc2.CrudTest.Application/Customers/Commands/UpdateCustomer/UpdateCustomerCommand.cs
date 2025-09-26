a721866aa3faa8a4872a65d3f535d2c2e5068ca5
fzleli <amirfzlelii@gmail.com>
2025-09-26 09:39:41 +0330

    feat(application): Add UpdateCustomerCommand with optional fields for updating customer data

---
+++ src/Mc2.CrudTest.Application/Customers/Commands/UpdateCustomer/UpdateCustomerCommand.cs
@@ -0,0 +1,16 @@
+﻿using Mc2.CrudTest.Domain.Entities;
+using MediatR;
+
+namespace Mc2.CrudTest.Application.Customers.Commands.UpdateCustomer
+{
+    public class UpdateCustomerCommand : IRequest<Customer>
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? BankAccountNumber { get; set; }
+    }
+}
