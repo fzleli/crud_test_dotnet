b17e8c70de2832286b29b423fe78a676de04e023
fzleli <amirfzlelii@gmail.com>
2025-09-26 14:06:11 +0330

    test(domain): Add test for Update Customer (CustomerTests, CustomerUpdatedEventTests)

--- tests/Mc2.CrudTest.Domain.UnitTests/Customers/CustomerTests.cs
+++ tests/Mc2.CrudTest.Domain.UnitTests/Customers/CustomerTests.cs
@@ -34,5 +34,40 @@ namespace Mc2.CrudTest.Domain.UnitTests.Customers
             createdEvent.Customer.FirstName.Should().Be(firstName);
             createdEvent.Customer.LastName.Should().Be(lastName);
         }
+
+        [Fact]
+        public void Update_ShouldReturnCustomer_AndRaiseEvent_WhenAllValid()
+        {
+            // Arrange
+            var firstName = "Amir";
+            var lastName = "Moradi";
+            var dob = new DateTime(1990, 5, 12);
+            var phone = "+989123456789";
+            var email = "test@example.com";
+            var bankAccount = "8205401026800208";
+            var customer = Customer.Create(firstName, lastName, dob, phone, email, bankAccount);
+
+            var updatedFirstName = "Vahid";
+            var updatedLastName = "Amiri";
+            var updatedDob = new DateTime(2000, 8, 07);
+
+            // Act
+            customer.Update(updatedFirstName, updatedLastName, updatedDob, phone, email, bankAccount);
+
+            // Assert
+            customer.FirstName.Should().Be(updatedFirstName);
+            customer.LastName.Should().Be(updatedLastName);
+            customer.DateOfBirth.Should().Be(updatedDob);
+
+            var domainEvents = customer.DomainEvents;
+            domainEvents.Should().HaveCount(2);
+            domainEvents.First().Should().BeOfType<CustomerCreatedEvent>();
+            domainEvents.Last().Should().BeOfType<CustomerUpdatedEvent>();
+
+            var updatedEvent = (CustomerUpdatedEvent)domainEvents.Last();
+            updatedEvent.Customer.Id.Should().Be(customer.Id);
+            updatedEvent.Customer.FirstName.Should().Be(updatedFirstName);
+            updatedEvent.Customer.LastName.Should().Be(updatedLastName);
+        }
     }
 }
---
+++ tests/Mc2.CrudTest.Domain.UnitTests/Customers/Events/CustomerUpdatedEventTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Mc2.CrudTest.Domain.Entities;
+using Mc2.CrudTest.Domain.Events;
+
+namespace Mc2.CrudTest.Domain.UnitTests.Customers.Events;
+
+public class CustomerUpdatedEventTests
+{
+    [Fact]
+    public void Constructor_ShouldSetProperties()
+    {
+        // Arrange
+        var sampleCustomer = Customer.Create(
+            "TestFirstName",
+            "TestLastName",
+            new DateTime(1990, 1, 1),
+            "+1234567890",
+            "test@example.com",
+            "1234567890123456"
+        );
+
+        // Act
+        var eventObj = new CustomerUpdatedEvent(sampleCustomer);
+
+        // Assert
+        eventObj.Customer.Should().Be(sampleCustomer);
+        eventObj.Customer.Id.Should().Be(sampleCustomer.Id);
+    }
+}
